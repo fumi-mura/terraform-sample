e8cd58cc742089a2922b6a7bda5b2b1d4a31da9c
fumi <381704fumi@gmail.com>
2024-09-12 01:48:33 +0900

    Change upper case

--- terraform/aws/environments/dev/iam/provider.tf
+++ terraform/aws/environments/dev/iam/provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   default_tags {
     tags = {
-      env               = local.env
-      created-terraform = true
+      Environment        = local.env
+      ManagedByTerraform = true
     }
   }
 }
