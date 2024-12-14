a26baacc25ca18b9e9b96a1c869046fd730e78a5
fumi <381704fumi@gmail.com>
2024-12-14 20:44:23 +0900

    Add user data

---
+++ terraform/aws/modules/ec2/scripts/user_data.sh
@@ -0,0 +1,23 @@
+# Execute user data when every restart ec2.
+# ref: https://repost.aws/ja/knowledge-center/execute-user-data-ec2
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+# cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+--//--
