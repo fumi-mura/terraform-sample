d23121ec31830284906a9e9c5bae9a22408aac51
fumi <381704fumi@gmail.com>
2024-12-27 20:39:22 +0900

    test plan

--- terraform/aws/environments/prd/main.tf
+++ terraform/aws/environments/prd/main.tf
@@ -12,3 +12,8 @@ module "s3_3" {
   source      = "../../modules/s3/bucket/"
   bucket_name = "${local.env}-${local.name}-test-s3-bucket-3"
 }
+
+module "s3_4" {
+  source      = "../../modules/s3/bucket/"
+  bucket_name = "${local.env}-${local.name}-test-s3-bucket-4"
+}
