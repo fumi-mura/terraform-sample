76861a74ddcc07d08820c325e8d010e426381a55
fumi <381704fumi@gmail.com>
2024-12-27 20:27:54 +0900

    test

--- terraform/aws/environments/dev/main.tf
+++ terraform/aws/environments/dev/main.tf
@@ -7,3 +7,8 @@ module "s3_2" {
   source      = "../../modules/s3/bucket/"
   bucket_name = "${local.env}-${local.name}-test-s3-bucket-2"
 }
+
+module "s3_3" {
+  source      = "../../modules/s3/bucket/"
+  bucket_name = "${local.env}-${local.name}-test-s3-bucket-3"
+}
