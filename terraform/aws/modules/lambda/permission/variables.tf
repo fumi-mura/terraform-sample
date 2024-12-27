6f88e3bf70ab729947b7eca159a1313a4c5b895e
fumi <381704fumi@gmail.com>
2024-12-28 03:57:11 +0900

    move permission

---
+++ terraform/aws/modules/lambda/permission/variables.tf
@@ -0,0 +1,15 @@
+variable "function_name" {
+  type = string
+}
+
+variable "statement_id" {
+  type = string
+}
+
+variable "principal" {
+  type = string
+}
+
+variable "source_arn" {
+  type = string
+}
