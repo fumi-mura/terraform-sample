f6cdbff74f0392a012e2f00592b0bf273694510c
fumi <381704fumi@gmail.com>
2024-12-27 21:58:23 +0900

    test ok

--- terraform/aws/environments/prd/main.tf
+++ terraform/aws/environments/prd/main.tf
@@ -2,23 +2,3 @@ module "s3_1" {
   source      = "../../modules/s3/bucket/"
   bucket_name = "${local.env}-${local.name}-test-s3-bucket-1"
 }
-
-module "s3_2" {
-  source      = "../../modules/s3/bucket/"
-  bucket_name = "${local.env}-${local.name}-test-s3-bucket-2"
-}
-
-module "s3_3" {
-  source      = "../../modules/s3/bucket/"
-  bucket_name = "${local.env}-${local.name}-test-s3-bucket-3"
-}
-
-module "s3_4" {
-  source      = "../../modules/s3/bucket/"
-  bucket_name = "${local.env}-${local.name}-test-s3-bucket-4"
-}
-
-module "s3_5" {
-  source      = "../../modules/s3/bucket/"
-  bucket_name = "${local.env}-${local.name}-test-s3-bucket-5"
-}
