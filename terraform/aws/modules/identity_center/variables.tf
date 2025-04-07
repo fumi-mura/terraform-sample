8979410e0171ed3275da50227a24b7ebc357a7a1
Fumi-Mura <381704fumi@gmail.com>
2025-04-08 01:50:49 +0900

    fmt

--- terraform/aws/modules/identity_center/variables.tf
+++ terraform/aws/modules/identity_center/variables.tf
@@ -1,24 +1,29 @@
 variable "env" {
   type = string
 }
+
 variable "name" {
   type = string
 }
+
 variable "permission_sets" {
   description = "Create IAM Identity Center permission set"
   type        = list(string)
   default     = ["AdministratorAccess", "ReadOnlyAccess"]
 }
+
 variable "create_users" {
   description = "Create IAM Identity Center users"
   type        = list(string)
   default     = ["Admin", "ReadOnly"]
 }
+
 variable "create_groups" {
   description = "Create IAM Identity Center group"
   type        = list(string)
   default     = ["Admin", "ReadOnly"]
 }
+
 variable "email_local_pert" {
   type = string
 }
