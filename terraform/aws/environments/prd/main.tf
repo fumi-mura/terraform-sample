9d42f269eb0322897737a0d24f1a8ae4d0fc8c68
fumi <381704fumi@gmail.com>
2025-04-10 18:18:18 +0900

    Make prd oidc role

---
+++ terraform/aws/environments/prd/main.tf
@@ -0,0 +1,46 @@
+# OIDC
+module "oidc_provider" {
+  source = "../../modules/iam/oidc"
+}
+
+module "oidc_iam_role" {
+  source               = "../../modules/iam/role"
+  iam_role_name        = "${local.env}-${local.name}-oidc-iam-role"
+  managed_policy_arns  = [module.oidc_iam_policy.iam_policy_arn]
+  max_session_duration = 3600
+  policy_statement = {
+    1 = {
+      effect  = "Allow"
+      actions = ["sts:AssumeRoleWithWebIdentity"]
+      principals = [{
+        type        = "Federated"
+        identifiers = ["${module.oidc_provider.oidc_arn}"]
+      }]
+      condition = [{
+        test     = "StringEquals"
+        variable = "token.actions.githubusercontent.com:aud"
+        values   = ["sts.amazonaws.com"]
+        },
+        {
+          test     = "StringLike"
+          variable = "token.actions.githubusercontent.com:sub"
+          values = [
+            "repo:Fumi-Mura/infra_portfolio:*",
+          ]
+      }]
+    }
+  }
+}
+
+module "oidc_iam_policy" {
+  source          = "../../modules/iam/policy"
+  iam_policy_name = "${local.env}-${local.name}-oidc-iam-policy"
+  policy_statement = {
+    1 = {
+      effect    = "Allow"
+      actions   = ["*"]
+      resources = ["*"]
+      condition = []
+    }
+  }
+}
