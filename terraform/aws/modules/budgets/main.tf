2b09d0ae731c57b9946ecb1f6682887d8c1fae7c
fumi <381704fumi@gmail.com>
2025-04-11 18:32:42 +0900

    Make budgets module for check action

---
+++ terraform/aws/modules/budgets/main.tf
@@ -0,0 +1,23 @@
+resource "aws_budgets_budget" "this" {
+  name         = var.budgets_name
+  budget_type  = "COST"
+  limit_amount = "1000"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  dynamic "notification" {
+    for_each = var.notification
+
+    content {
+      comparison_operator        = "GREATER_THAN"
+      threshold                  = 100
+      threshold_type             = "PERCENTAGE"
+      notification_type          = notification.value.notification_type
+      subscriber_email_addresses = notification.value.email_addresses
+    }
+  }
+
+  tags = {
+    Nmae = var.budgets_name
+  }
+}
