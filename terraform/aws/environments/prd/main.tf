50523540f1d796942ae5f94d5610d5b650df9195
fumi <63145482+fumi-mura@users.noreply.github.com>
2025-04-16 15:11:59 +0900

    Test trivy scan (#31)
    * Test trivy scan
    
    * Use trivyignore
    
    * Use config
    
    * Roleback
    
    * Note
    
    * test
    
    * Roleback
    
    * test
    
    * Roleback
    
    * test
    
    * Roleback

--- terraform/aws/environments/prd/main.tf
+++ terraform/aws/environments/prd/main.tf
@@ -49,8 +49,3 @@ module "test_1_s3" {
   source      = "../../modules/s3/bucket"
   bucket_name = "${local.env}-${local.name}-test-1-s3-bucket"
 }
-
-module "test_2_s3" {
-  source      = "../../modules/s3/bucket"
-  bucket_name = "${local.env}-${local.name}-test-2-s3-bucket"
-}
