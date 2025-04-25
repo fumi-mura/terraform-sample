475a9ad0c00efbe63524eaddb869e01bfa9261ab
Fumi-Mura <381704fumi@gmail.com>
2025-04-25 19:44:44 +0900

    Make backup module

---
+++ terraform/aws/modules/backup/variables.tf
@@ -0,0 +1,34 @@
+variable "plan_name" {
+  type = string
+}
+
+variable "rule_name" {
+  type = string
+}
+
+variable "vault_name" {
+  type = string
+}
+
+variable "account_id" {
+  type = string
+}
+
+variable "selection_name" {
+  type = string
+}
+
+variable "iam_role_arn" {
+  type = string
+}
+
+variable "resources" {
+  type = list(string)
+}
+
+variable "conditions" {
+  type = map(object({
+    key   = string
+    value = string
+  }))
+}
