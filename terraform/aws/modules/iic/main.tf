ff776e73cf10829d3d3dd1a431c1d890151d77fb
Fumi-Mura <381704fumi@gmail.com>
2025-04-25 16:20:55 +0900

    Position change

--- terraform/aws/modules/iic/main.tf
+++ terraform/aws/modules/iic/main.tf
@@ -1,21 +1,5 @@
 data "aws_ssoadmin_instances" "this" {}
 
-resource "aws_ssoadmin_permission_set" "this" {
-  for_each = var.permission_sets
-
-  name         = each.value.name
-  description  = each.value.description
-  instance_arn = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-}
-
-resource "aws_ssoadmin_managed_policy_attachment" "this" {
-  for_each = var.permission_sets
-
-  instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  managed_policy_arn = each.value.managed_policy_arn
-  permission_set_arn = aws_ssoadmin_permission_set.this[each.key].arn
-}
-
 # User
 resource "aws_identitystore_user" "this" {
   for_each = var.users
@@ -51,6 +35,22 @@ resource "aws_identitystore_group_membership" "this" {
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
   group_id          = aws_identitystore_group.this[each.value["group"]].group_id
   member_id         = aws_identitystore_user.this[each.value["user"]].user_id
+}
+
+resource "aws_ssoadmin_permission_set" "this" {
+  for_each = var.permission_sets
+
+  name         = each.value.name
+  description  = each.value.description
+  instance_arn = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "this" {
+  for_each = var.permission_sets
+
+  instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+  managed_policy_arn = each.value.managed_policy_arn
+  permission_set_arn = aws_ssoadmin_permission_set.this[each.key].arn
 }
 
 # Apply IIC settings to each account
