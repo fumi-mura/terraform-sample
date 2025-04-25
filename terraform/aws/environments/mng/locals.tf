ba44544b17e2d024890e07809a1ce05d713960d2
Fumi-Mura <381704fumi@gmail.com>
2025-04-25 14:13:09 +0900

    Add locals

--- terraform/aws/environments/mng/locals.tf
+++ terraform/aws/environments/mng/locals.tf
@@ -4,3 +4,20 @@ locals {
   env  = "mng"
   name = "fumis-pf"
 }
+
+locals {
+  users = {
+    Admin = {
+      name = {
+        family_name = "Admin"
+        given_name  = "Admin"
+      }
+    }
+    ReadOnly = {
+      name = {
+        family_name = "ReadOnly"
+        given_name  = "ReadOnly"
+      }
+    }
+  }
+}
