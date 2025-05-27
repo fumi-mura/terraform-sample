b9795324e2a61b54095c0b9c100d0ec00cebe9f5
fumi <63145482+fumi-mura@users.noreply.github.com>
2025-05-27 14:10:34 +0900

    Lambda module修正 (#43)
    * Add source_code_hash
    
    * Ignore build artifact
    
    * fmt
    
    * Fix dynamic

--- terraform/aws/modules/lambda/main.tf
+++ terraform/aws/modules/lambda/main.tf
@@ -5,17 +5,18 @@ data "archive_file" "this" {
 }
 
 resource "aws_lambda_function" "this" {
-  function_name = var.funcion_name
-  role          = var.role_arn
-  filename      = data.archive_file.this.output_path
-  handler       = var.handler
-  runtime       = var.runtime
-  memory_size   = var.memory_size
-  timeout       = var.timeout
-  layers        = var.layers
+  function_name    = var.funcion_name
+  role             = var.role_arn
+  filename         = data.archive_file.this.output_path
+  source_code_hash = data.archive_file.this.output_base64sha256
+  handler          = var.handler
+  runtime          = var.runtime
+  memory_size      = var.memory_size
+  timeout          = var.timeout
+  layers           = var.layers
 
   dynamic "environment" {
-    for_each = length(keys(var.environment_variables)) == 0 ? 0 : 1
+    for_each = length(keys(var.environment_variables)) == 0 ? [] : [true]
     content {
       variables = var.environment_variables
     }
