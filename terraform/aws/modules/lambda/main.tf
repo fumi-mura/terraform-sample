260be87a768d0ceb2cda3140fb086a7bf11ff53a
fumi <381704fumi@gmail.com>
2025-05-27 14:03:42 +0900

    Add source_code_hash

--- terraform/aws/modules/lambda/main.tf
+++ terraform/aws/modules/lambda/main.tf
@@ -8,6 +8,7 @@ resource "aws_lambda_function" "this" {
   function_name = var.funcion_name
   role          = var.role_arn
   filename      = data.archive_file.this.output_path
+  source_code_hash = data.archive_file.this.output_base64sha256
   handler       = var.handler
   runtime       = var.runtime
   memory_size   = var.memory_size
