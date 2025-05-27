03d8743c49072fd3d89687f3e36e61ad2ef7d7d8
fumi <381704fumi@gmail.com>
2025-05-27 14:04:27 +0900

    fmt

--- terraform/aws/modules/lambda/main.tf
+++ terraform/aws/modules/lambda/main.tf
@@ -5,15 +5,15 @@ data "archive_file" "this" {
 }
 
 resource "aws_lambda_function" "this" {
-  function_name = var.funcion_name
-  role          = var.role_arn
-  filename      = data.archive_file.this.output_path
+  function_name    = var.funcion_name
+  role             = var.role_arn
+  filename         = data.archive_file.this.output_path
   source_code_hash = data.archive_file.this.output_base64sha256
-  handler       = var.handler
-  runtime       = var.runtime
-  memory_size   = var.memory_size
-  timeout       = var.timeout
-  layers        = var.layers
+  handler          = var.handler
+  runtime          = var.runtime
+  memory_size      = var.memory_size
+  timeout          = var.timeout
+  layers           = var.layers
 
   dynamic "environment" {
     for_each = length(keys(var.environment_variables)) == 0 ? 0 : 1
